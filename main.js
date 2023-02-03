e5d32ca0ae4862efff40fdaaf00e9288ae224d49
Shahinur Rahman Shahin <srshahin111@gmail.com>
2023-02-03 18:19:08 +0600

    old project

---
+++ main.js
@@ -0,0 +1,20 @@
+// <!-- Design and Wordpress section -->
+		// <div class="design-sec py-5"> 
+			// <div class="design-box py-3" style="width: 92%; margin: auto;"> 
+				// <div class="row"> 
+					// <div class="col-lg-6">
+						// <div class="dev-content m-auto" style="width: 92%; padding-top: 70px"> 
+							// <h1 class="py-4">We make Static Website and Wordpress</h1>
+							// <p class="pb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua. Ut enim ad minim. Sed do eiusmod tempor incididunt . Get our Service.</p>
+							// <button type="button" class="btn btn-primary px-4 mr-2">Get Start</button>
+							// <button type="button" class="btn btn-warning px-4 mr-2">Sign Up</button>
+						// </div>
+					// </div>
+					// <div class="col-lg-6">
+						// <div class="dev-img" style="padding-top: 50px;">
+							// <img src="images/web-icon.png" class="d-block m-auto" alt="" />
+						// </div>
+					// </div>
+				// </div>
+			// </div>
+		// </div>
